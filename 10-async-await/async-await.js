db1b6fd34a831adb71d96710a5bdff9269202e26
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-08-28 19:48:50 -0600

    Promises and AsyncAwait

---
+++ 10-async-await/async-await.js
@@ -0,0 +1,44 @@
+// La palabra async  la convierte inmediatamente en asíncrona.
+asyncfunctionhola(nombre) {
+    returnnew Promise(function (resolve, reject) {
+            setTimeout(function () {
+                console.log('Hola, '+ nombre);
+                resolve(nombre);
+            }, 1500);
+        });
+    }
+    
+    asyncfunctionhablar(nombre) {
+    returnnew Promise( (resolve, reject) => {
+            setTimeout(function() {
+                console.log('Bla bla bla bla...');
+                resolve('Hay un error');
+            }, 1000);
+        });
+    }
+    
+    asyncfunctionadios(nombre) {
+    returnnew Promise( (resolve, reject) => {
+            setTimeout(function() {
+                console.log('Adios', nombre);
+                resolve();
+            }, 1000);
+        });
+    }
+    
+    
+    // Await solo es válido dentro de una función asíncrona.
+    asyncfunctionmain() {
+    let nombre =await hola('Alejandro');
+    await hablar();
+        hablar(); // Para hacer que se ejecute en segundo plano no debe existi el await
+    await hablar();
+    await adios(nombre);
+        console.log('Termina el proceso');
+    }
+    
+    // Esto nos permitirá saber si nuestra función se está ejecutanod de forma asíncrona.
+    console.log('Empezamos el proceso');
+    main();
+    console.log('Va a ser la segunda instrucción')
+    
